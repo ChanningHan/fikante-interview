d46410891c47da269e66154762f0ed89f8fb5225
hanyuanlin <690051834@qq.com>
2021-10-11 09:19:43 +0800

    剑指offer 26 树的子结构

---
+++ packages/algorithm/剑指offer题目/26树的子结构.js
@@ -0,0 +1,72 @@
+/*
+ *
+ *输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+
+B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+
+例如:
+给定的树 A:
+
+  3
+  / \
+ 4  5
+ / \
+1  2
+给定的树 B：
+
+ 4
+ /
+1
+返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
+
+示例 1：
+
+输入：A = [1,2,3], B = [3,1]
+输出：false
+示例 2：
+
+输入：A = [3,4,5,1,2], B = [4,1]
+输出：true
+限制：
+
+0 <= 节点个数 <= 10000
+
+ *
+ * */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} A
+ * @param {TreeNode} B
+ * @return {boolean}
+ */
+function isSubStructure(A, B) {
+    if (A === null || B == null) {
+        return false;
+    }
+
+    function doseTree1HaveTree2(treeNode1, treeNode2) {
+        if (treeNode2 === null) return true;
+        if (treeNode1 === null) return false;
+        if (!(treeNode1.val === treeNode2.val)) return false;
+        return (
+            doseTree1HaveTree2(treeNode1.left, treeNode2.left) &&
+            doseTree1HaveTree2(treeNode1.right, treeNode2.right)
+        );
+    }
+
+    let res;
+    if (A.val === B.val) {
+        res = doseTree1HaveTree2(A, B);
+    }
+    if (!res) res = isSubStructure(A.left, B);
+    if (!res) res = isSubStructure(A.right, B);
+
+    return res;
+}
