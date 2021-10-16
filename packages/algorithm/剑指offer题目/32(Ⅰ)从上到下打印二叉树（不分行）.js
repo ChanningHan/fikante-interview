822e4e0cd12c95e9c824c28138e30be1048ee692
ChanningHan <690051834@qq.com>
2021-10-16 11:19:09 +0800

    剑指offer 32 题目2 从上到下打印二叉树（分行)

---
+++ packages/algorithm/剑指offer题目/32(Ⅰ)从上到下打印二叉树（不分行）.js
@@ -0,0 +1,79 @@
+/**
+ * 
+ * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+
+ 
+
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其层次遍历结果：
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+ 
+
+提示：
+
+节点总数 <= 1000
+
+ * 
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if (!root) return []
+    const queue = []
+    const res = [
+            []
+        ]
+        // 当前行未打印数量
+    let current = 0
+        // 下一行未打印数量
+    let next = 0
+
+    queue.push(root)
+    current = 1
+    while (queue.length) {
+        const node = queue.shift();
+        res[res.length - 1].push(node.val)
+        current--
+
+        if (node.left) {
+            queue.push(node.left)
+            next++
+        }
+        if (node.right) {
+            queue.push(node.right)
+            next++
+        }
+
+        if (!current) {
+            next && res.push([])
+            current = next
+            next = 0
+        }
+
+    }
+
+    return res
+};
