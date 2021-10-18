5d585505bc44e11286df4df689daa9aa4a9ddefa
hanyuanlin <690051834@qq.com>
2021-10-18 17:07:05 +0800

    剑指offer 39 数组中出现次数超过一半的数字（快速排序法2）

--- packages/algorithm/剑指offer题目/39数组中出现次数超过一半的数字.js
+++ packages/algorithm/剑指offer题目/39数组中出现次数超过一半的数字.js
@@ -44,12 +44,13 @@ function checkMoreThanHalf(nums, number) {
 }
 
 /**
+ * @description 快排分组方法（创建left、right数组）
  * @param {number[]} nums
  * @param {number} start
  * @param {number} end
  * @return {number}
  * */
-function partition(nums, start, end) {
+function partition1(nums, start, end) {
     if (start >= end) {
         return start;
     }
@@ -74,6 +75,47 @@ function partition(nums, start, end) {
     });
     return resIndex;
 }
+
+// 交换两个元素
+function swap(nums, i, j) {
+    const temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+function getRandom(start, end) {
+    return Math.floor(Math.random() * (end - start + 1) + start);
+}
+
+/**
+ * @description 快排分组方法2（时间复杂度O(1)，不创建额外数组）
+ * @param {number[]} nums
+ * @param {number} start
+ * @param {number} end
+ * @return {number}
+ * */
+function partition(nums, start, end) {
+    // 指向第一个大于middleNumber的数的指针
+    let j = start - 1;
+    const middleIndex = ((end - start + 1) >> 1) + start;
+    swap(nums, middleIndex, end);
+
+    for (let i = start; i < end; i++) {
+        if (nums[i] >= nums[end]) {
+            if (j < start) {
+                j = i;
+            }
+        } else if (j >= start) {
+            swap(nums, i, j);
+            j++;
+        }
+    }
+
+    j >= start && swap(nums, j, end);
+
+    return j >= start ? j : start;
+}
+
 /**
  * @description 解法一，使用类似快速排序的算法。
  * 核心思路是如果一个数的次数大于数组的一半长度，那么它一定是这个数组的中位数。
@@ -102,8 +144,8 @@ const majorityElement = function (nums) {
             index = partition(nums, start, end);
         }
     }
-    if (checkMoreThanHalf(nums, nums[index])) return nums[middle];
-    return nums[middle];
+    if (checkMoreThanHalf(nums, nums[index])) return nums[index];
+    return nums[index];
 };
 
 function test(fn) {
@@ -120,7 +162,27 @@ function test(fn) {
             input: [2, 2],
             output: 2,
         },
-    ].slice(2);
+        {
+            input: [
+                47, 47, 72, 47, 72, 47, 79, 47, 12, 92, 13, 47, 47, 83, 33, 15, 18, 47, 47, 47, 47,
+                64, 47, 65, 47, 47, 47, 47, 70, 47, 47, 55, 47, 15, 60, 47, 47, 47, 47, 47, 46, 30,
+                58, 59, 47, 47, 47, 47, 47, 90, 64, 37, 20, 47, 100, 84, 47, 47, 47, 47, 47, 89, 47,
+                36, 47, 60, 47, 18, 47, 34, 47, 47, 47, 47, 47, 22, 47, 54, 30, 11, 47, 47, 86, 47,
+                55, 40, 49, 34, 19, 67, 16, 47, 36, 47, 41, 19, 80, 47, 47, 27,
+            ],
+            output: 47,
+        },
+        {
+            input: [
+                12, 52, 12, 70, 12, 61, 12, 12, 50, 72, 82, 12, 11, 25, 28, 43, 40, 12, 12, 53, 12,
+                12, 98, 12, 12, 92, 81, 2, 12, 15, 40, 12, 12, 9, 12, 31, 12, 12, 12, 12, 77, 12,
+                12, 50, 12, 12, 12, 93, 41, 92, 12, 12, 12, 12, 12, 12, 12, 12, 12, 37, 48, 14, 12,
+                70, 82, 12, 80, 12, 12, 12, 12, 56, 30, 12, 8, 12, 50, 12, 20, 12, 21, 97, 12, 42,
+                12, 10, 12, 38, 73, 15, 9, 11, 79, 12, 12, 28, 51, 12, 15, 12,
+            ],
+            output: 12,
+        },
+    ].slice(0);
     testArr.forEach((item, index) => {
         console.log(`test${index + 1},${JSON.stringify(item)}`);
         const res = fn(item.input);
