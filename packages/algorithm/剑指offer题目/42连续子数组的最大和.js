d0c55900980bee37445496ff91d8847261cdbf35
hanyuanlin <690051834@qq.com>
2021-10-22 09:36:47 +0800

    剑指offer 42 连续子数组的最大和（解法1）

---
+++ packages/algorithm/剑指offer题目/42连续子数组的最大和.js
@@ -0,0 +1,84 @@
+/*
+ *
+ *输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+
+要求时间复杂度为O(n)。
+
+
+
+示例1:
+
+输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+输出: 6
+解释:连续子数组[4,-1,2,1] 的和最大，为6。
+
+
+提示：
+
+1 <=arr.length <= 10^5
+-100 <= arr[i] <= 100
+
+ *
+ * */
+
+function test(fn) {
+    const testArr = [
+        {
+            input: [-2, 1, -3, 4, -1, 2, 1, -5, 4],
+            output: 6,
+        },
+        {
+            input: [-2, -1],
+            output: -1,
+        },
+        {
+            input: [-1, 1, 2, 1],
+            output: 4,
+        },
+    ];
+
+    let passCount = 0;
+    testArr.forEach((item, index) => {
+        console.log(`test${index + 1}`);
+        console.log(JSON.stringify(item));
+        const res = fn(item.input);
+        console.log('res', res);
+        const isPass = res === item.output;
+        isPass && passCount++;
+        console.log(isPass ? 'pass' : 'fail');
+    });
+    console.log(`total pass: ${passCount} / ${testArr.length}`);
+}
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function maxSubArray(nums) {
+    if (!nums.length) return 0;
+    if (nums.length === 1) return nums[0];
+    let maxValue = nums[0];
+    let count = 0;
+    nums.forEach((num) => {
+        if (maxValue < 0 && num > maxValue) {
+            maxValue = num;
+            count = num;
+            return;
+        }
+
+        if (num >= 0) {
+            count += num;
+            if (count > maxValue) {
+                maxValue = count;
+            }
+        } else if (count + num <= 0) {
+            count = 0;
+        } else {
+            count += num;
+        }
+    });
+
+    return maxValue;
+}
+
+test(maxSubArray);
