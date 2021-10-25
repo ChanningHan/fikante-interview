a60a7c713efa25024b8f57759742d78c56809765
hanyuanlin <690051834@qq.com>
2021-10-25 10:57:17 +0800

    剑指offer 46 把数字翻译成字符串（动态规划）

--- packages/algorithm/剑指offer题目/45把数组排成最小的数.js
+++ packages/algorithm/剑指offer题目/45把数组排成最小的数.js
@@ -2,17 +2,17 @@
  * 
  * 输入一个非负整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
 
- 
+
 
 示例 1:
 
 输入: [10,2]
 输出: "102"
-示例 2:
+示例2:
 
 输入: [3,30,34,5,9]
 输出: "3033459"
- 
+
 
 提示:
 
@@ -25,14 +25,13 @@
  * 
  */
 
-
 /**
  * @description 定义compare中两个数m、n的大小关系: 如果mn<nm，则 定义m小于n，m应该排在n前面
  * @param {number[]} nums
  * @return {string}
  */
-var minNumber = function(nums) {
-    return nums.sort((a, b) => a + '' + b > b + '' + a ? 1 : -1).join('')
+let minNumber = function (nums) {
+    return nums.sort((a, b) => (`${a}${b}` > `${b}${a}` ? 1 : -1)).join('');
 };
 
 console.log(minNumber([3, 30, 34, 5, 9]));
---
+++ packages/algorithm/剑指offer题目/46把数字翻译成字符串.js
@@ -0,0 +1,47 @@
+/*
+ *
+ *给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+
+
+
+示例 1:
+
+输入: 12258
+输出: 5
+解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
+
+
+提示：
+
+0 <= num < 231
+
+ *
+ * */
+
+/**
+ * @description 动态规划，f(n) = f(n-1) + g(n,n-1) * f(n-2)；
+ * g(n,n-1)表示n和n-1加起来是否在10~25之间，若是则返回1否则返回0，这里大于或等于10是因为不允许前置0的情况，比如01。
+ *
+ * @param {number} num
+ * @return {number}
+ */
+function translateNum(num) {
+    const numStr = num.toString();
+    if (numStr.length === 1) return 1;
+
+    function isLegal(index1, index2) {
+        const composition = numStr[index1] + numStr[index2] - 0;
+        return composition >= 10 && composition <= 25 ? 1 : 0;
+    }
+
+    const countArr = [1];
+    countArr[1] = isLegal(0, 1) + countArr[0];
+    for (let i = 2; i < numStr.length; i++) {
+        countArr[i] = countArr[i - 1] + isLegal(i - 1, i) * countArr[i - 2];
+    }
+
+    return countArr[countArr.length - 1];
+}
+
+console.log(translateNum(1112));
+console.log(translateNum(12258));
