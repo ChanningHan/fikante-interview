46ef55f70de19aad8e7ff46702f0615116d43449
hanyuanlin <690051834@qq.com>
2021-10-29 09:47:16 +0800

    剑指offer 55（II）平衡二叉树
    解法1

---
+++ packages/algorithm/剑指offer题目/55（II）平衡二叉树.js
@@ -0,0 +1,67 @@
+/*
+*
+* 输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。
+
+
+
+示例 1:
+
+给定二叉树 [3,9,20,null,null,15,7]
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回 true 。
+
+示例 2:
+
+给定二叉树 [1,2,2,3,3,null,null,4,4]
+
+       1
+      / \
+     2   2
+    / \
+   3   3
+  / \
+ 4   4
+返回false 。
+
+
+
+限制：
+
+0 <= 树的结点个数 <= 10000
+
+*
+* */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+function isBalanced(root) {
+    function treeDeep(node) {
+        if (!node) return 0;
+        const leftTreeDeep = treeDeep(node.left);
+        const rightTreeDeep = treeDeep(node.right);
+        return Math.max(leftTreeDeep, rightTreeDeep) + 1;
+    }
+
+    if (!root) return true;
+
+    const leftTreeDeep = treeDeep(root.left);
+    const rightTreeDeep = treeDeep(root.right);
+    if (Math.abs(leftTreeDeep - rightTreeDeep) > 1) {
+        return false;
+    }
+
+    return isBalanced(root.left) && isBalanced(root.right);
+}
