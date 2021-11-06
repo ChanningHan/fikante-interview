9725150be4fabcb29f91f372296e568ff5c887f6
ChanningHan <690051834@qq.com>
2021-11-06 12:01:53 +0800

    复习剑指offer 31~35

--- packages/algorithm/剑指offer题目/33二叉搜索树的后序遍历序列.js
+++ packages/algorithm/剑指offer题目/33二叉搜索树的后序遍历序列.js
@@ -1,6 +1,7 @@
 /**
  * 
- * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。如果是则返回 true，否则返回 false。假设输入的数组的任意两个数字都互不相同。
+ * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。
+ * 如果是则返回 true，否则返回 false。假设输入的数组的任意两个数字都互不相同。
 
  
 
@@ -41,6 +42,8 @@ var verifyPostorder = function(postorder) {
         if (postorder[leftIndex] > root) {
             break;
         }
+
+
     }
 
 
