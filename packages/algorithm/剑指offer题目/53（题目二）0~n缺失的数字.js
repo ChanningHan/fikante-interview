b339aff5af63cadfe7d7b62c22b2a0130cccef2a
ChanningHan <690051834@qq.com>
2021-11-07 12:10:27 +0800

    复习剑指offer 51~55

--- packages/algorithm/剑指offer题目/53（题目二）0~n缺失的数字.js
+++ packages/algorithm/剑指offer题目/53（题目二）0~n缺失的数字.js
@@ -1,6 +1,7 @@
 /*
  *
- *一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+ *一个长度为n-1的递增排序数组中的所有数字都是唯一的，
+ 并且每个数字都在范围0～n-1之内。在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
 
 
 
@@ -29,8 +30,9 @@
  * @param {number[]} nums
  * @return {number}
  */
-let missingNumber = function (nums) {
+let missingNumber = function(nums) {
     if (!nums || !nums.length) return 0;
+
     function findMissingNumber(start, end) {
         if (start > end) return start;
         const mid = ((end - start) >> 1) + start;
