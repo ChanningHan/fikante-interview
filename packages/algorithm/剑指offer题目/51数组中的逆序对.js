c0fe815ee76adf9d64d21a2d8b4b02de74d4314d
ChanningHan <690051834@qq.com>
2021-11-07 11:19:03 +0800

    复习剑指offer 45~50

--- packages/algorithm/剑指offer题目/51数组中的逆序对.js
+++ packages/algorithm/剑指offer题目/51数组中的逆序对.js
@@ -1,6 +1,7 @@
 /*
  *
- *在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数。
+ *在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。
+ 输入一个数组，求出这个数组中的逆序对的总数。
 
 
 
@@ -22,7 +23,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-let reversePairs = function (nums) {
+let reversePairs = function(nums) {
     if (!nums || !nums.length || nums.length === 1) return 0;
 
     let count = 0;
